071f55b8bb1ef1262bda0c6204623a69eff519a7
João Martins <joao.figueiredo.martins@canonical.com>
2021-07-28 09:04:06 -0400

    Add endpoints to retrieve recruitment data
    Add an endpoint to retrieve current applications for jobs the logged in person is responsible for.
    Add an endpoint to retrieve previous interviews for the logged in person.

---
+++ webapp/database.py
@@ -0,0 +1,18 @@
+import os
+
+import psycopg2
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from webapp.models import Base
+
+
+greenhouse_connection = lambda: psycopg2.connect(
+    os.getenv("GREENHOUSE_DATABASE_URL")
+)
+
+
+engine = create_engine(os.getenv("CANONICAL_DATABASE_URL"))
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+db_session = Session()
--- webapp/migrate.py
+++ webapp/migrate.py
@@ -1,10 +1,7 @@
 import csv
 import os
-import psycopg2
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
-from webapp.models import Base, Employee
+from webapp.database import greenhouse_connection, db_session
 from webapp.functions import (
     add_new_candidates,
     get_jobs,
@@ -18,48 +15,38 @@ from webapp.functions import (
     scorecard_added,
     stage_moved,
 )
+from webapp.models import Employee
 
 
-# Connect to greenhouse database
-greenhouse_connection = psycopg2.connect(os.getenv("GREENHOUSE_DATABASE_URL"))
-greenhouse_cursor = greenhouse_connection.cursor()
+with greenhouse_connection() as connection:
+    greenhouse_cursor = connection.cursor()
 
-# Connect to our database
-engine = create_engine(os.getenv("CANONICAL_DATABASE_URL"))
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    import_employees(greenhouse_cursor, db_session)
 
+    hiring_leads = {}
+    with open("hiring_leads.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for row in reader:
+            if not row["Primary Recruiter"]:
+                continue
 
-import_employees(greenhouse_cursor, session)
+            try:
+                employee = (
+                    db_session.query(Employee)
+                    .filter(Employee.full_name == row["Primary Recruiter"])
+                    .one_or_none()
+                )
+                hiring_leads[int(row["Job ID"])] = employee.id
+            except:
+                continue
 
-hiring_leads = {}
-with open("hiring_leads.csv", "r") as csvfile:
-    reader = csv.DictReader(csvfile, skipinitialspace=True)
-    for row in reader:
-        if not row["Primary Recruiter"]:
-            continue
-
-        try:
-            employee = (
-                session.query(Employee)
-                .filter(Employee.full_name == row["Primary Recruiter"])
-                .one_or_none()
-            )
-            hiring_leads[int(row["Job ID"])] = employee.id
-        except:
-            continue
-
-get_jobs(greenhouse_cursor, session, hiring_leads)
-add_new_candidates(greenhouse_cursor, session)
-import_candidate_applications(greenhouse_cursor, session)
-import_candidate_rejections(greenhouse_cursor, session)
-import_candidate_hired(greenhouse_cursor, session)
-import_cv_reviewed(greenhouse_cursor, session, hiring_leads)
-interview_scheduled(greenhouse_cursor, session)
-participated_interview(greenhouse_cursor, session)
-scorecard_added(greenhouse_cursor, session)
-stage_moved(greenhouse_cursor, session)
-
-# Close connection
-greenhouse_connection.close()
+    get_jobs(greenhouse_cursor, db_session, hiring_leads)
+    add_new_candidates(greenhouse_cursor, db_session)
+    import_candidate_applications(greenhouse_cursor, db_session)
+    import_candidate_rejections(greenhouse_cursor, db_session)
+    import_candidate_hired(greenhouse_cursor, db_session)
+    import_cv_reviewed(greenhouse_cursor, db_session, hiring_leads)
+    interview_scheduled(greenhouse_cursor, db_session)
+    participated_interview(greenhouse_cursor, db_session)
+    scorecard_added(greenhouse_cursor, db_session)
+    stage_moved(greenhouse_cursor, db_session)
