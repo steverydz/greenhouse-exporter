f39c51bca9b9913a987fab5c6812db2a69355450
Thomas Bille <toto@canonical.com>
2021-08-06 15:20:02 +0200

    Add GitHub checks and lint configurations;

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    dataLayer: "readonly",
+    d3: "readonly",
+    topojson: "readonly",
+    ga: "readonly",
+    grecaptcha: "readonly",
+    serialize: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: ["@babel/plugin-syntax-jsx", "@typescript-eslint"],
+      presets: ["@babel/preset-react"],
+    },
+  },
+  rules: {
+    semi: ["error", "always"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "no-prototype-builtins": "off",
+  },
+};
